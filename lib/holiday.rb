08c6aa47dccad6a211df7681466d5c5eeecdae0e
Katie Cassell <kcassell944@gmail.com>
2018-02-28 17:45:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,9 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash[:fourth_of_july][:memorial_day]
-  holiday_hash
+  holiday_hash.each do |season, holidays|
+    binding.pry
+  end
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
